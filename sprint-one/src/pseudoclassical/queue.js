2341865fa0a5f20f4839721da3f5359e67d9698a
bkillenit <blaine.j.killen@gmail.com>
2015-08-19 13:44:55 -0700

    all tests for the pseudoclassical implementation of queue and stack now pass

--- sprint-one/src/pseudoclassical/queue.js
+++ sprint-one/src/pseudoclassical/queue.js
@@ -1,6 +1,27 @@
 var Queue = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  this.storage = {'size': 0};
+  this.curPoint = 0;
 };
 
+Queue.prototype.enqueue = function(value){
+  if(value === undefined) return;
 
+  this.storage[this.storage['size']] = value;
+  this.storage.size += 1;
+}
+
+Queue.prototype.dequeue = function(){
+  if( this.size() === 0) return;
+
+  var answer = this.storage[this.curPoint];
+  delete this.storage[this.curPoint];
+
+  this.curPoint += 1;
+  return answer;
+}
+
+Queue.prototype.size = function(){
+  return this.storage.size - this.curPoint;
+}
--- sprint-one/src/pseudoclassical/stack.js
+++ sprint-one/src/pseudoclassical/stack.js
@@ -1,6 +1,25 @@
 var Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  this.storage = {'size': 0};
 };
 
+Stack.prototype.push = function(value){
+  if(value === undefined) return;
 
+  this.storage[this.size()] = value;
+  this.storage['size'] += 1;
+}
+
+Stack.prototype.pop = function(){
+  if(this.size() === 0) return;
+  this.storage.size -= 1;
+
+  var answer = this.storage[this.size()];
+  delete this.storage[this.size()];
+  return answer;
+}
+
+Stack.prototype.size = function(){
+  return this.storage.size;
+}
