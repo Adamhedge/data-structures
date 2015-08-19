2fab2aa8fdad7b7e3bf3e93f824af304d7464dbb
bkillenit <blaine.j.killen@gmail.com>
2015-08-19 13:24:21 -0700

    all test for the function shared versions of queue and stack pass

--- sprint-one/src/functional-shared/queue.js
+++ sprint-one/src/functional-shared/queue.js
@@ -1,8 +1,33 @@
 var Queue = function(){
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  var someInstance = {};
+  
+  someInstance.storage = {'size': 0};
+  someInstance.curPoint = 0;
+
+  return _.extend( someInstance, queueMethods );
 };
 
-var queueMethods = {};
+var queueMethods = {  // Implement the methods below
+  enqueue: function(value){
+    if(value === undefined) return;
+
+    this.storage[this.storage['size']] = value;
+    this.storage.size += 1;
+  },
+  dequeue : function(){
+    if( this.size() === 0) return;
+
+    var answer = this.storage[this.curPoint];
+    delete this.storage[this.curPoint];
+
+    this.curPoint += 1;
+    return answer;
+  },
+  size : function(){
+    return this.storage.size - this.curPoint;
+  }
+};
 
 
--- sprint-one/src/functional-shared/stack.js
+++ sprint-one/src/functional-shared/stack.js
@@ -1,8 +1,30 @@
 var Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  var someInstance = {};
+  someInstance.storage = {'size': 0};
+
+  return _.extend(someInstance, stackMethods);
 };
 
-var stackMethods = {};
+var stackMethods = {
+	push : function(value){
+    if(value === undefined) return;
 
+    this.storage[this.size()] = value;
+    this.storage['size'] += 1;
+  },
 
+  pop : function(){
+    if(this.size() === 0) return;
+    this.storage.size -= 1;
+
+    var answer = this.storage[this.size()];
+    delete this.storage[this.size()];
+    return answer;
+  },
+
+  size : function(){
+    return this.storage.size;
+  }
+};
