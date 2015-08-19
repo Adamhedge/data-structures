ce0c041551a62ac6983a6195c759ad5a996ec4c2
bkillenit <blaine.j.killen@gmail.com>
2015-08-19 12:34:11 -0700

    all tests for stack and queue for the functional pattern now pass

--- sprint-one/src/functional/queue.js
+++ sprint-one/src/functional/queue.js
@@ -2,17 +2,32 @@ var Queue = function(){
   var someInstance = {};
 
   // Use an object with numeric keys to store values
-  var storage = {};
+  var storage = {'size': 0};
+  var valsAdded = 0;
+  var curPoint = 0;
 
   // Implement the methods below
-
   someInstance.enqueue = function(value){
+    if(value === undefined) return;
+
+    storage[storage['size']] = value;
+    storage.size += 1;
+    valsAdded += 1;
   };
 
   someInstance.dequeue = function(){
+    if( valsAdded === 0 ) return;
+
+    var answer = storage[curPoint];
+    delete storage[curPoint];
+
+    curPoint += 1;
+    valsAdded -= 1;
+    return answer;
   };
 
   someInstance.size = function(){
+    return valsAdded;
   };
 
   return someInstance;
--- sprint-one/src/functional/stack.js
+++ sprint-one/src/functional/stack.js
@@ -2,16 +2,27 @@ var Stack = function(){
   var someInstance = {};
 
   // Use an object with numeric keys to store values
-  var storage = {};
+  var storage = {'size':0};
 
   // Implement the methods below
   someInstance.push = function(value){
+    if(value === undefined) return;
+
+    storage[storage.size] = value;
+    storage['size'] += 1;
   };
 
   someInstance.pop = function(){
+    if(storage['size'] === 0) return;
+    storage.size -= 1;
+    
+    var answer = storage[storage.size];
+    delete storage[storage.size];
+    return answer;
   };
 
   someInstance.size = function(){
+    return storage.size;
   };
 
   return someInstance;
