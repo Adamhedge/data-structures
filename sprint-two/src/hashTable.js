ca08481eddf87ab5398e65b9a810667f7f9270a0
adamhedge <adamhedge@gmail.com>
2015-08-19 17:12:55 -0700

    all tests in hash table class now pass

--- sprint-two/src/hashTable.js
+++ sprint-two/src/hashTable.js
@@ -5,15 +5,42 @@ var HashTable = function(){
 
 HashTable.prototype.insert = function(k, v){
   var i = getIndexBelowMaxForKey(k, this._limit);
+  if(this._storage.get(i) === undefined) this._storage.set(i, [[k,v]]);
+  else {
+    var collisions = this._storage.get(i);
+
+    collisions.forEach(function(key, index){
+      if(key[0] === k){ 
+        collisions.splice(index, 1);
+      }
+    });
+
+    if(collisions === undefined) collisions = [];
+
+    collisions.push([k,v]);
+    this._storage.set(i, collisions );
+  }
 };
 
 HashTable.prototype.retrieve = function(k){
   var i = getIndexBelowMaxForKey(k, this._limit);
+  var collisions = this._storage.get(i);
+  var answer = null;
 
+  if( collisions !== undefined && collisions !== null ) {
+    collisions.forEach(function(val){
+      var key = val[0];
+      if(k === key) answer = val[1];
+    });
+  }
+
+  return answer;
 };
 
 HashTable.prototype.remove = function(k){
+  var i = getIndexBelowMaxForKey(k, this._limit);
 
+  this._storage.set(i, null);
 };
 
 
