e7a40650bab18c4ec5cc1146ef9b11a93c57b594
adamhedge <adamhedge@gmail.com>
2015-08-20 10:51:45 -0700

    doubly linked list pseduo implemented

---
+++ extra-credit/spec/doublyLinkedListSpec.js
@@ -0,0 +1,38 @@
+describe('doublyLinkedList', function() {
+  var list;
+
+  beforeEach(function() {
+    list = new DoublyLinkedList();
+    list.addToHead(4);
+    list.addToHead(5);
+    list.addToHead(6);
+  });
+
+  it('should have methods named "addChild" and "contains", and a property named "value"', function() {
+    expect(list.addToHead).to.be.a("function");
+    expect(list.removeTail).to.be.a("function");
+    expect(list.contains).to.be.a("function");
+    expect(list.removeNthElement).to.be.a("function");
+  });
+
+  it('should designate a new head when new nodes are added to head', function() {
+    expect(list.tail.value).to.equal(4);
+    expect(list.tail.value).to.equal(4);
+    expect(list.head.value).to.equal(6);
+    expect(list.head.next.previous.value).to.equal(6);
+  });
+
+  it('should maintain the linked list when removing from tail', function() {
+    expect(list.removeTail()).to.equal(4);
+    expect(list.contains(4)).to.equal(false);
+    expect(list.contains(5)).to.equal(true);
+  });
+
+  // it('should remove the nth element from the list', function() {
+  //   list.addToHead(7);
+  //   list.addToHead(8);
+  //   expect(list.removeNthElement(3)).to.equal(6);
+  //   expect(list.contains(6)).to.equal(false);
+  //   expect(list.contains(5)).to.equal(true);
+  // });
+});
---
+++ extra-credit/src/doublyLinkedList.js
@@ -0,0 +1,78 @@
+debugger;
+var DoublyLinkedList = function(){
+    this.head = null;
+    this.tail = null;
+    this.size = 0;
+};
+
+DoublyLinkedList.prototype.addToHead = function(value) {
+  var newNode = new Node(value);
+  
+  if(this.head === null) {
+    this.head = newNode;
+    this.tail = newNode;
+  } else {
+    var head = this.head;
+    this.head = newNode;
+    this.head.next = head;
+    this.head.next.previous = this.head;
+  }
+
+  this.size += 1;
+}
+
+DoublyLinkedList.prototype.removeTail = function() {
+  if(this.cantRemove()){ return; }
+
+  var oldTail = this.tail.value
+
+  this.tail = this.tail.previous;
+  this.tail.next = null;
+
+  this.size -= 1;
+
+  return oldTail;
+}
+
+DoublyLinkedList.prototype.contains = function(value) {
+  var pointer = this.head;
+
+  while( pointer !== null ) {
+    if(value === pointer.value){ return true; }
+  }
+
+  return false;
+}
+
+// DoublyLinkedList.prototype.removeNthElement = function(nth) {
+//   if(this.cantRemove()){ return; }
+//   else if(this.size < nth){ return; }
+
+//   var direction = (this.size / 2) < nth ? 'previous' : 'next'; 
+//   var pointer = direction === 'previous' ? this.tail : this.head;
+//   var curNth = direction === 'previous' ? this.size : 0;
+
+//   while(pointer !== null) {
+//     curNth = direction === 'previous' ? curNth-1 : curNth+1;
+//     if( curNth === nth ) {
+//       pointer.next.previous = pointer.previous;
+//       pointer.previous.next = pointer.next;
+//       return pointer.value;
+//     }
+//     else { pointer = pointer[direction]; }
+//   }
+// }
+
+// DoublyLinkedList.prototype.cantRemove = function() {
+//   return this.size === 0 ? true : false;
+// }
+
+var Node = function(value){
+  var node = {};
+
+  node.value = value;
+  node.next = null;
+  node.previous = null;
+
+  return node;
+};
