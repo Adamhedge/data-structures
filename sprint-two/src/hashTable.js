9522ac8f3ca5b479ce2515bd34daf8caa0d4c490
adamhedge <adamhedge@gmail.com>
2015-08-21 11:03:43 -0700

    refactored HashTable class, remove now works correctly, abided by OOP best practices.

--- sprint-two/src/hashTable.js
+++ sprint-two/src/hashTable.js
@@ -8,6 +8,7 @@ var HashTable = function(){
   this._insertAllToHashTable = function() {
     var oldStorage = this._storage;
     this._storage = LimitedArray(this._limit);
+    this._size = 0;
     var hashClass = this;
 
     oldStorage.each.call(hashClass ,function(elem){
@@ -18,8 +19,7 @@ var HashTable = function(){
         var k = kvPair[0];
         var v = kvPair[1];
 
-        var index = getIndexBelowMaxForKey(k, this._limit);
-        this.insert.call(this, index, v);
+        this.insert.call(this, k, v);
       }
     });
   }
@@ -37,9 +37,9 @@ var HashTable = function(){
   this._checkTableSize = function(op) {
     // checks to see if the amount of keys used has exceeded 75% of the amount of keys available in the hash table
     // and then checks to see if the amount of keys falls below 25% of the amount of keys available in the hash table
-    if(this._size >= ((this._limit) * .75).toFixed(1) && op === 'insert') {
+    if(op === 'insert' && this._size >= ((this._limit) * .75).toFixed(1)) {
       this._increaseHashTable.call(this, this._resizingFactor);
-    } else if(this._size < ((this._limit) * .25).toFixed(1) && op === 'remove') {
+    } else if(op === 'remove' && this._size < ((this._limit) * .25).toFixed(1)) {
       this._decreaseHashTable.call(this, this._resizingFactor);
     }
   }
@@ -68,7 +68,8 @@ HashTable.prototype.insert = function(k, v){
 
   var i = getIndexBelowMaxForKey(k, this._limit);
   insertAndHandleCollisions.call(this._storage, i, v);
-  this._keysUsed++;
+
+  this._size++;
   this._checkTableSize.call(this, 'insert');
 };
 
@@ -109,7 +110,7 @@ HashTable.prototype.remove = function(k){
 
   this._storage.set(i, collisions);
 
-  this._keysUsed--;
+  this._size--;
   this._checkTableSize.call(this, 'remove');
   
   return answer;
