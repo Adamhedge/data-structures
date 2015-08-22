21c7ebd22a8a439de7606130072fb94b1771b29a
adamhedge <adamhedge@gmail.com>
2015-08-22 10:42:35 -0700

    AVL tree implemented and all tests pass.

--- extra-credit/spec/avlTreeSpec.js
+++ extra-credit/spec/avlTreeSpec.js
@@ -2,19 +2,30 @@ describe('avlTree', function() {
   var avlTree;
 
   beforeEach(function() {
-    avlTree = new AVLTree(2);
+    avlTree = new AVLTree(1);
   });
 
-  it('should have methods named "insert" and "contains"', function() {
+  it('should have methods named "insert", "leftRotation", "rightRotation", and "contains"', function() {
     expect(avlTree.insert).to.be.a("function");
+    expect(avlTree.leftRotation).to.be.a("function");
+    expect(avlTree.rightRotation).to.be.a("function");
     expect(avlTree.contains).to.be.a("function");
   });
 
-  it('should contain values entered in the bloom filter', function() {
-    expect(avlTree.contains('Brendan')).to.equal(true);
+  it('should contain values entered in the AVL Tree', function() {
+    avlTree.insert(2);
+    avlTree.insert(3);
+    avlTree.insert(4);
+    expect(avlTree.contains(4)).to.equal(true);
   });
 
-  it('should not contain values not entered in the bloomm filter, with a 90% false positive rate', function() {
-    expect(avlTree.contains('NoobKiller')).to.equal(false);
+  it('should rebalance the tree when -1 < balanceFactor > 1', function() {
+    avlTree.insert(2);
+    avlTree.insert(3);
+    avlTree.insert(4);
+    expect(avlTree.right.right.value).to.equal(4);
+    expect(avlTree.right.left).to.equal(null);
+    expect(avlTree.left.value).to.equal(1);
+    expect(avlTree.value).to.equal(2);
   });
 });
--- extra-credit/src/avlTree.js
+++ extra-credit/src/avlTree.js
@@ -3,25 +3,67 @@ var AVLTree = function(value) {
   this.left = null;
   this.right = null;
   this.balanceFactor = [0,0];
+}
 
+AVLTree.prototype.updateBalanceFactors = function(pointer) {
+  var leftHeight;
+  var rightHeight;
+
+  if(pointer.left !== null) {
+    pointer.updateBalanceFactors(pointer.left);
+    pointer.balanceFactor[0] = Math.max(pointer.left.updateBalanceFactors[0], pointer.left.updateBalanceFactors[1]) + 1;
+  } else {
+    pointer.balanceFactor[0] = 0;
+  }
+
+  if(pointer.right !== null) {
+    pointer.updateBalanceFactors(pointer.right);
+    pointer.balanceFactor[1] = Math.max(pointer.right.updateBalanceFactors[0], pointer.right.updateBalanceFactors[1]) + 1;
+  } else {
+    pointer.balanceFactor[1] = 0;
+  }
+}
+
+AVLTree.prototype.contains = function( value ) {
+  if(this.value === value) return true;
+  else if((value < this.value)) {
+    if(this.left === null) {
+      return false;
+    } else {
+      return this.left.contains(value);
+    }
+  }
+  else {
+    if(this.right === null){
+      return false;
+    } else {
+      return this.right.contains(value);
+    }
+  }
 }
 
 AVLTree.prototype.leftRotation = function(pointer, parent) {
-  var tempLeft = parent;
+  //var tempLeft = parent;
+  var tempLeft = new AVLTree(parent.value);
+  tempLeft.left = parent.left;
   tempLeft.right = pointer.left;
 
   parent.value = pointer.value;
   parent.left = tempLeft;
   parent.right = pointer.right;
+
+  pointer.updateBalanceFactors(pointer);
 }
 
 AVLTree.prototype.rightRotation = function(pointer, parent) {
-  var tempRight = parent;
+  var tempRight = JSON.parse(JSON.stringify(parent));
   tempRight.left = pointer.right;
 
   parent.value = pointer.value;
   parent.left = pointer.left;
   parent.right = tempRight;
+
+  pointer.updateBalanceFactors(pointer);
 }
 
 AVLTree.prototype.insert = function(value) {
@@ -43,17 +85,28 @@ AVLTree.prototype.insert = function(value) {
       return;
     }
 
-    pointer.balanceFactor[0] = max(pointer.left.balanceFactor[0], pointer.left.balanceFactor[1]) + 1;
-    pointer.balanceFactor[1] = max(pointer.right.balanceFactor[0], pointer.right.balanceFactor[1]) + 1;
+    if(pointer.left !== null) {
+      pointer.balanceFactor[0] = Math.max(pointer.left.balanceFactor[0], pointer.left.balanceFactor[1]) + 1;
+    }
 
-    var leftHeight = this.balanceFactor[0];
+    if(pointer.right !== null) {
+      pointer.balanceFactor[1] = Math.max(pointer.right.balanceFactor[0], pointer.right.balanceFactor[1]) + 1;
+    }
+
+    var leftHeight = pointer.balanceFactor[0];
     var rightHeight = pointer.balanceFactor[1];
     var balanceFactor = leftHeight - rightHeight;
 
     if(balanceFactor > 1) {
-      rightRotation(pointer, parent);
+      if(pointer == parent) {
+        pointer = parent.left;
+      }
+      pointer.rightRotation(pointer, parent);
     } else if(balanceFactor < -1) {
-      leftRotation(pointer, parent);
+      if(pointer == parent) {
+        pointer = parent.right;
+      }
+      pointer.leftRotation(pointer, parent);
     }
   }
 
