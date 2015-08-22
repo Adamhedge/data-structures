08b65b7c66f847e3929e8173062a0c1a3f69cdc0
adamhedge <adamhedge@gmail.com>
2015-08-21 22:01:44 -0700

    directed graph class implemented with respective tests

---
+++ extra-credit/spec/directedGraphSpec.js
@@ -0,0 +1,31 @@
+describe('directedGraph', function() {
+  var graph;
+
+  beforeEach(function() {
+    graph = new DirectedGraph();
+  });
+
+  it('should have methods named "insertEdge", "removeEdge", and "nodePointsToNodes"', function() {
+    expect(graph.insertEdge).to.be.a("function");
+    expect(graph.removeEdge).to.be.a("function");
+    expect(graph.nodePointsToNodes).to.be.a("function");
+  });
+
+  it('should correctly store the weights for each edge', function() {
+    graph.insertEdge(1,3,1);
+    graph.insertEdge(5,1);
+    graph.insertEdge(4,2);
+    
+    expect(graph.containsEdge(4,2)).to.equal(true);
+    expect(graph.containsEdge(1,5)).to.equal(false);
+  });
+
+  it('should correctly return all the nodes that a node points to', function() {
+    graph.insertEdge(1,2);
+    graph.insertEdge(1,3);
+    graph.insertEdge(1,4);
+    console.log(graph.nodePointsToNodes(1));
+    expect(_.isEqual(graph.nodePointsToNodes(1), [[2,1],[3,1],[4,1]])).to.equal(true);
+    expect(_.isEqual(graph.nodePointsToNodes(1), [1])).to.equal(false);
+  });
+});
--- extra-credit/src/directedGraph.js
+++ extra-credit/src/directedGraph.js
@@ -1,28 +1,67 @@
 var DirectedGraph = function() {
 	// adjacency matrix representation for the graph
 	this.graph = [[]];
-}
+};
 
 DirectedGraph.prototype.insertEdge = function(fromNode, toNode, weight) {
-	if(weight === undefined) weight = 0;
+	fromNode--;
+	toNode--;
+
+	if(weight === undefined) weight = 1;
+
+	if(this.graph[fromNode] === undefined) {
+		this.graph[fromNode] = [];
+	}
 
 	this.graph[fromNode][toNode] = weight;
-}
+};
 
 DirectedGraph.prototype.removeEdge = function(fromNode, toNode) {
+	fromNode--;
+	toNode--;
+
 	var oldNode = this.graph[fromNode][toNode];
 	this.graph[fromNode][toNode] = undefined;
 
 	return oldNode;
-}
+};
+
+// need to refactor ds, if node doesnt point outwards, then contains will be O(n)
+DirectedGraph.prototype.containsNode = function(node) {
+	node--;
+
+	if(this.graph[node] !== undefined) { 
+		return true; 
+	}
+
+	// accounts for cyclic graphs as well
+	for(var i = 0; i < node; i++) {
+		if(this.graph[i][node] !== undefined) { 
+			return true; 
+		}
+	}
+
+	return false;
+};
+
+DirectedGraph.prototype.containsEdge = function(fromNode, toNode) {
+	fromNode--;
+	toNode--;
+
+	return (this.graph[fromNode][toNode] !== undefined);
+};
 
 DirectedGraph.prototype.nodePointsToNodes = function(node) {
+	node--;
+
 	var buffer = [];
 
-	for(var i = 0; i<graph[node].length; i++) {
-		var weight = graph[node][i];
-		if(weight !== undefined) buffer.push([i, weight]);
+	for(var i = 0; i<this.graph[node].length; i++) {
+		var weight = this.graph[node][i];
+		if(weight !== undefined) { 
+			buffer.push([i+1, weight]); 
+		}
 	}
 
 	return buffer;
-}
+};
